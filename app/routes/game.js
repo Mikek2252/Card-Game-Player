1b7b218cd2d822ab1a08d56a69417e7e26e996cc
mikek2252 <michael.r.kerr1@gmail.com>
2019-10-26 13:38:28 +0200

    ISSUE-2: feat: added model for deck and updated game context model

--- app/routes/game.js
+++ app/routes/game.js
@@ -3,7 +3,10 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     return this.store.createRecord('game-context', {
-      playerCount: 2
+      playerCount: 2,
+      deck: this.store.createRecord('deck', {
+        cardCount: 10
+      })
     })
   }
 });
