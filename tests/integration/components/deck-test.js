410c222d324e7f6b9bc69c7d084b79222f437cc6
mikek2252 <mikek2252@btinternet.com>
2019-11-23 13:58:41 +0000

    ISSUE-18: feat: translation files and update to templates

--- tests/integration/components/deck-test.js
+++ tests/integration/components/deck-test.js
@@ -2,11 +2,13 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { setupIntl } from 'ember-intl/test-support';
 import EmberObject from "@ember/object";
 import sinon from 'sinon';
 
 module('Integration | Component | deck', function(hooks) {
   setupRenderingTest(hooks);
+  setupIntl(hooks, 'en-gb');
 
   test('Data renders in component', async function(assert) {
 
